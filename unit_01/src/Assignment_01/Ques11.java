298e387b95bd80e5127f0eef5eca62e7dfc1e793
Dell <Dell@DESKTOP-LCCD1F1>
2022-05-14 10:30:13 +0530

    practical

--- unit_01/src/Assignment_01/Ques11.java
+++ unit_01/src/Assignment_01/Ques11.java
@@ -1,5 +1,44 @@
 package Assignment_01;
 
-public class Ques11 {
+import java.util.Scanner;
 
+public class Ques11 {
+	public static void main( String[] args ) {
+		
+		Scanner obj = new Scanner( System.in );
+		
+		System.out.print("Enter the first number         : ");
+		int a = obj.nextInt();
+		
+		System.out.print("Enter the second number        : ");
+		int b = obj.nextInt();
+		
+		test1 obj1 = new test1( a , b );
+		
+		int result = ( a > b ) ? obj1.first() : obj1.second();
+		
+		System.out.println("The result of the operation is : "+result);
+		obj.close();
+	}
+}
+
+class test1{
+	int a , b;
+	
+	test1(){
+		
+	}
+	
+	test1( int a , int b ){
+		this.a = a;
+		this.b = b;
+	}
+	
+	int first() {
+		return ( a + b );
+	}
+	
+	int second() {
+		return ( ( a - b ) < 0 ) ? 0 : ( a - b );
+	}
 }
